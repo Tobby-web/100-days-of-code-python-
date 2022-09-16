7ed41109b4361b18ed9bbb5e2acc8406e7648edd
Tobechukwu <agbasieretobeckwu@gmail.com>
2022-09-16 07:03:28 -0400

    update to code

--- bandname_generator.py
+++ bandname_generator.py
@@ -2,4 +2,4 @@
 print("Welcome to the Band Name generator")
 city = input("What's the name of the city you grew in?\n ")
 pet = input("What is the name of your pet?\n")
-print(f"Your band name is {city} {pet}")
+print(f"Your band name could be {city} {pet}")

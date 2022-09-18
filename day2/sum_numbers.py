7898297060c3dd10c3717042b7fbbd5054c18d4a
Tobechukwu <agbasieretobeckwu@gmail.com>
2022-09-17 21:50:44 -0400

    complete and correct code

--- day2/sum_numbers.py
+++ day2/sum_numbers.py
@@ -8,9 +8,8 @@ length_of_number = len(number)
 
 
 #create a for loop that runs through the list, converts to string and adds them to a variable
-
+sum = 0
 for i in number:
-	sum = 0
 	n = int(i)
-	sum = sum + n
+	sum += n
 print(sum)

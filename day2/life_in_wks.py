0e2d4848c4ee5932c6a2b35d5bf2bb988b3e4591
Tobechukwu <agbasieretobeckwu@gmail.com>
2022-09-18 20:30:22 -0400

    calculate your lifespan in years, weeks, months and weeks

---
+++ day2/life_in_wks.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+age = float(input("How old are you? "))
+
+years_left = 90 - age
+
+months_left = years_left * 12
+
+weeks_left = years_left * 52
+
+days_left = years_left * 365
+
+print(f"YOu have the {years_left} years left to live. That means you have {weeks_left} weeks or {days_left} days left.\n Our assumptions are that you have a lifespan of 90 years. Dark!!")
